b6251b90ce526a6f59a866cf7fb876f01897afb3
Ngurah Kresna <ngurahkresna99@gmail.com>
2020-03-29 11:41:45 +0700

    add : layout and adapters checkout

---
+++ app/src/main/java/com/example/gambungstore/CheckoutDone.java
@@ -0,0 +1,14 @@
+package com.example.gambungstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckoutDone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_done);
+    }
+}
