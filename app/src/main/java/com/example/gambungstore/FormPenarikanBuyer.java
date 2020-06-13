e9b259a02ed0e3e18f76bc55168e51c6a96f5547
renalnurrachman <renaldrachman@gmail.com>
2020-06-13 14:01:53 +0700

    Menambah XML Penarikan Jicash Buyer

---
+++ app/src/main/java/com/example/gambungstore/FormPenarikanBuyer.java
@@ -0,0 +1,14 @@
+package com.example.gambungstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormPenarikanBuyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_penarikan_buyer);
+    }
+}
---
+++ app/src/main/java/com/example/gambungstore/NotifikasiJumlahPenarikanGagal.java
@@ -0,0 +1,14 @@
+package com.example.gambungstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotifikasiJumlahPenarikanGagal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifikasi_jumlah_penarikan_gagal);
+    }
+}
