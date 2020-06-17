5d0815dcd99b551c22bf1460184cab8d824462ac
renalnurrachman <renaldrachman@gmail.com>
2020-06-17 20:31:41 +0700

    Menambah fungsi java

--- app/src/main/java/com/example/gambungstore/CheckoutPenarikanBuyerProses.java
+++ app/src/main/java/com/example/gambungstore/CheckoutPenarikanBuyerProses.java
@@ -4,17 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class CheckoutPenarikanBuyerProses extends AppCompatActivity {
 
     private ImageView mBackArrow;
+    private TextView mTime;
+    private CountDownTimer timerClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_penarikan_buyer_proses);
+
+        mTime = findViewById(R.id.time);
+        timerClass = new CountDownTimer(60000 * 60 *24, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                String waktu = String.format("%02d:%02d:%02d",
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) -
+                                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
+                mTime.setText("" +waktu);
+            }
+
+            @Override
+            public void onFinish() {
+                ///Berjalan saat waktu telah selesai atau berhenti
+                Toast.makeText(CheckoutPenarikanBuyerProses.this, "Waktu Telah Berakhir", Toast.LENGTH_LONG).show();
+                mTime.setText("00:00:00");
+                finish();
+            }
+        }.start();
 
         mBackArrow = findViewById(R.id.backArrow);
         mBackArrow.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +54,8 @@ public class CheckoutPenarikanBuyerProses extends AppCompatActivity {
             }
         });
     }
+
+
 
     public void btnHome(View view) {
         finish();
@@ -37,4 +68,5 @@ public class CheckoutPenarikanBuyerProses extends AppCompatActivity {
         Intent intent = new Intent(CheckoutPenarikanBuyerProses.this,NotifikasiBerhasil.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/example/gambungstore/FormPenarikanBuyer.java
+++ app/src/main/java/com/example/gambungstore/FormPenarikanBuyer.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class FormPenarikanBuyer extends AppCompatActivity {
 
+    private EditText mJumlahjicash, mNomorrekening, mAtasnama, mPenyediajasa;
     private ImageView mBackArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_penarikan_buyer);
+
+        this.getXml();
 
         mBackArrow = findViewById(R.id.backArrow);
         mBackArrow.setOnClickListener(new View.OnClickListener() {
@@ -25,9 +31,34 @@ public class FormPenarikanBuyer extends AppCompatActivity {
             }
         });
     }
+    private void getXml(){
+        this.mJumlahjicash = findViewById(R.id.jumlahjicash);
+        this.mNomorrekening = findViewById(R.id.nomorrekening);
+        this.mAtasnama = findViewById(R.id.atasnama);
+        this.mPenyediajasa = findViewById(R.id.penyediajasa);
+    }
 
     public void Konfirmasiformbtn(View view) {
-        Intent intent = new Intent(FormPenarikanBuyer.this, CheckoutPenarikanBuyerProses.class);
-        startActivity(intent);
+
+        if(mJumlahjicash.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "Pastikan Jumlah Jicash Field Terisi",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(mNomorrekening.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "Pastikan Nomor Rekening Field Terisi",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(mAtasnama.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "Pastikan Atas Nama Field Terisi",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(mPenyediajasa.getText().toString().isEmpty()){
+            Toast.makeText(getApplicationContext(), "Pastikan Penyedia Jasa Field Terisi",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+            Intent intent = new Intent(FormPenarikanBuyer.this, CheckoutPenarikanBuyerProses.class);
+            startActivity(intent);
+        }
     }
 }
